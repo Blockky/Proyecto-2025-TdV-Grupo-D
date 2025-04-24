022ff7f0918207e140352c0d5833fc2dad67ae68
Riverfan <luka.luraschi@edu.uah.es>
2025-04-24 16:09:40 +0200

    archivo tsx puzzle

---
+++ Puzzle.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Puzzle" tilewidth="32" tileheight="32" tilecount="32" columns="8">
+ <image source="Tiles/Puzzle_PH.png" width="256" height="128"/>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="8" width="32" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8" y="0" width="16" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="8" width="32" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="30">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2" y="2" width="28" height="28"/>
+  </objectgroup>
+ </tile>
+ <tile id="31">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8" y="0" width="16" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
